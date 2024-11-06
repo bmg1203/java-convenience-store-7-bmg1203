10be764b91905e838f04a11f2d06d8842e2d84db
bmg1203 <bmg1203@naver.com>
2024-11-07 03:57:40 +0900

    [feat]: Promotions 클래스 추가
    - 프로모션들을 저장합니다.
    - 특정 프로모션이 기간 내인지 확인할 수 있습니다.

---
+++ src/main/java/store/domain/Promotions.java
@@ -0,0 +1,22 @@
+package store.domain;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Promotions {
+
+    private final Map<String, Promotion> promotions = new HashMap<>();
+
+    public Promotions(List<Promotion> promotionInput) {
+        for (Promotion promotion : promotionInput) {
+            promotions.put(promotion.getName(), promotion);
+        }
+    }
+
+    public boolean isWithinPeriod(String name) {
+        Promotion promotion = promotions.get(name);
+        return promotion.isActive();
+    }
+}
