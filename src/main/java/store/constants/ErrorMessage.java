c37b26f341f3c334da7c3893e276d4e9d91acf9d
bmg1203 <bmg1203@naver.com>
2024-11-07 05:40:51 +0900

    [feat]: 구입 수량, 중복 상품 에러 메시지 추가
    - 구입 수량은 1 이상이어야 합니다.
    - 같은 상품은 한 번에 입력 받습니다.

--- src/main/java/store/constants/ErrorMessage.java
+++ src/main/java/store/constants/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     PRODUCT_BUY_FORM_ERROR("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     NO_EXSIST_PRODUCT_ERROR("존재하지 않는 상품 입니다. 다시 입력해 주세요"),
     NOT_ENOUGH_QUANTITY_ERROR("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    NOT_POSITIVE_INPUT_ERROR("구입할 수량은 1 이상이어야 합니다. 다시 입력해 주세요."),
     ETC_INPUT_ERROR("잘못된 입력입니다. 다시 입력해주세요."),
+    DUPLICATE_PRODUCT_INPUT_ERROR("같은 상품은 한 번에 여러개 구입합니다. 다시 입력해 주세요."),
     PRODUCT_ERROR("잘못된 재고입니다.");
 
     private final String prefix = "[ERROR] ";
