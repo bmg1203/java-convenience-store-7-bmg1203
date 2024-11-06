82576ad8f95e1c5ffd18e6ef320152d7442fc449
bmg1203 <bmg1203@naver.com>
2024-11-07 00:49:53 +0900

    [feat]: Product parse와 유효성 기능 추가
    - 문자로 받은 수량과 가격을 숫자로 변환합니다.
    - 수량과 가격의 최소 수량과 가격을 유효성 검사 합니다.

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,20 +1,61 @@
 package store.domain;
 
+import store.constants.ErrorMessage;
+import store.constants.NumberConstants;
+
 public class Product {
 
-    private String name;
-    private int price;
-    private int quantity;
-    private String promotion;
+    private final String name;
+    private final int price;
+    private final int quantity;
+    private final String promotion;
 
-    public Product(String name, int price, int quantity, String promotion) {
+    public Product(String name, String price, String quantity, String promotion) {
         this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+        this.price = validatePrice(parseToInt(price));
+        this.quantity = validateQuantity(parseToInt(quantity));
         this.promotion = promotion;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    private int parseToInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.PRODUCT_ERROR.getMessage());
+        }
+    }
+
+    private int validateQuantity(int quantity) {
+        if(quantity < NumberConstants.MIN_QUANTITY.getCount()) {
+            throw new IllegalArgumentException(ErrorMessage.PRODUCT_ERROR.getMessage());
+        }
+        return quantity;
+    }
+
+    private int validatePrice(int price) {
+        if(price < NumberConstants.MIN_PRICE.getCount()) {
+            throw new IllegalArgumentException(ErrorMessage.PRODUCT_ERROR.getMessage());
+        }
+        return price;
+    }
+
     public boolean hasPromition() {
-        return promotion != null && promotion.equals("null");
+        return promotion != null && !promotion.equals("null");
     }
 }
