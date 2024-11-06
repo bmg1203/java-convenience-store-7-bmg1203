e54f9e28aeff1555f95bdafdf7488d9966fb6ffb
bmg1203 <bmg1203@naver.com>
2024-11-07 03:34:10 +0900

    [feat]: Products 재고 관리 클래스 추가
    - 프로모션이 있는 재고
    - 프로모션이 없는 재고
    - 각각 재고 관리 기능
    - 존재하는 상품 확인 기능

---
+++ src/main/java/store/domain/Products.java
@@ -0,0 +1,52 @@
+package store.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import store.constants.ErrorMessage;
+
+public class Products {
+
+    private final Map<String, Product> promotionProducts = new HashMap<>();
+    private final Map<String, Product> regularProducts = new HashMap<>();
+
+    public Products(List<Product> allProducts) {
+        for (Product product : allProducts) {
+            addProduct(product);
+        }
+    }
+
+    public Map<String, Product> getPromotionProducts() {
+        return promotionProducts;
+    }
+
+    public Map<String, Product> getRegularProducts() {
+        return regularProducts;
+    }
+
+    private void addProduct(Product product) {
+        if (product.hasPromotion()) {
+            promotionProducts.put(product.getName(), product);
+        }
+        if (!product.hasPromotion()) {
+            regularProducts.put(product.getName(), product);
+        }
+    }
+
+    public boolean hasProduct(String productName) {
+        if (!promotionProducts.containsKey(productName) && !regularProducts.containsKey(productName)) {
+            throw new IllegalArgumentException(ErrorMessage.NO_EXSIST_PRODUCT_ERROR.getMessage());
+        }
+        return promotionProducts.containsKey(productName) || regularProducts.containsKey(productName);
+    }
+
+    public void updatePromotionProductQuantitu(String name, int quantity) {
+        Product product = promotionProducts.get(name);
+        product.updateQuantity(quantity);
+    }
+
+    public void updateRegularProductQuantitu(String name, int quantity) {
+        Product product = regularProducts.get(name);
+        product.updateQuantity(quantity);
+    }
+}
