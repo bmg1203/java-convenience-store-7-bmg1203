3484cb414fd096d83beb7a3b0e1829b8037ceb2a
bmg1203 <bmg1203@naver.com>
2024-11-07 00:42:53 +0900

    [feat]: 재고 에러 메시지 추가
    - 재고가 잘 못 되었을 때 메시지를 출력합니다.

--- src/main/java/store/constants/ErrorMessage.java
+++ src/main/java/store/constants/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     PRODUCT_BUY_FORM_ERROR("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     NO_EXSIST_PRODUCT_ERROR("존재하지 않는 상품 입니다. 다시 입력해 주세요"),
     NOT_ENOUGH_QUANTITY_ERROR("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
-    ETC_INPUT_ERROR("잘못된 입력입니다. 다시 입력해주세요.");
+    ETC_INPUT_ERROR("잘못된 입력입니다. 다시 입력해주세요."),
+    PRODUCT_ERROR("잘못된 재고입니다.");
 
     private final String prefix = "[ERROR] ";
     private final String message;
