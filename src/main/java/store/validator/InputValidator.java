ffc849ebdfbc61d8020555615bb75ee85b4b630f
bmg1203 <bmg1203@naver.com>
2024-11-08 04:46:59 +0900

    [feat]: InputValidator 구입 상품 입력 유효성 검사 추가
    - 구입하는 항목이 상품에 있는지 확인하는 validateHasProduct
    - 구입하는 항목의 재고가 충분한지 확인하는 validateSufficientStock

--- src/main/java/store/validator/InputValidator.java
+++ src/main/java/store/validator/InputValidator.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import store.constants.ErrorMessage;
+import store.domain.Cart;
+import store.domain.Products;
+import store.domain.Purchase;
 
 public class InputValidator {
 
@@ -16,6 +19,22 @@ public class InputValidator {
 
             if (!matcher.matches()) {
                 throw new IllegalArgumentException(ErrorMessage.PRODUCT_BUY_FORM_ERROR.getMessage());
+            }
+        }
+    }
+
+    public static void validateHasProduct(Cart cart, Products products) {
+        for (Purchase purchase : cart.getItems()) {
+            if (!products.hasProduct(purchase.getName())) {
+                throw new IllegalArgumentException(ErrorMessage.NO_EXSIST_PRODUCT_ERROR.getMessage());
+            }
+        }
+    }
+
+    public static void validateSufficientStock(Cart cart, Products products) {
+        for (Purchase purchase : cart.getItems()) {
+            if (!products.hasSufficientStock(purchase.getName(), purchase.getQuantity())) {
+                throw new IllegalArgumentException(ErrorMessage.NO_EXSIST_PRODUCT_ERROR.getMessage());
             }
         }
     }
