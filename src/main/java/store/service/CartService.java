bd18464484892aa3a44fc6d5a4436f81040ba0cd
bmg1203 <bmg1203@naver.com>
2024-11-07 23:04:37 +0900

    [feat]: CartService 클래스에 재고 확인 메소드 추가
    - 구매항목의 재고를 확인합니다.
    - 프로모션과 일반재고를 각각 확인합니다.

---
+++ src/main/java/store/service/CartService.java
@@ -0,0 +1,47 @@
+package store.service;
+
+import store.constants.ErrorMessage;
+import store.domain.Cart;
+import store.domain.Product;
+import store.domain.Products;
+import store.domain.Promotions;
+import store.domain.Purchase;
+import store.view.InputView;
+
+public class CartService {
+
+    public static void hasProduct(Cart cart, Products products) {
+        for (Purchase purchase : cart.getItems()) {
+            if (!products.hasProduct(purchase.getName())) {
+                System.out.println(ErrorMessage.NO_EXSIST_PRODUCT_ERROR.getMessage());
+                InputView.readItem();
+            }
+        }
+    }
+
+    public static void hasQuantity(Cart cart, Products products, Promotions promotions) {
+        for (Purchase purchase : cart.getItems()) {
+            if (products.getPromotionProducts().containsKey(purchase.getName())) {
+                checkPromotionQuantity(purchase, products, promotions);
+                continue;
+            }
+            if (products.getRegularProducts().containsKey(purchase.getName()) {
+                checkRegularQuantity(purchase, products);
+            }
+        }
+    }
+
+    private static void checkPromotionQuantity(Purchase purchase, Products products, Promotions promotions) {
+        Product promotionProduct = products.getPromotionProducts().get(purchase.getName());
+        if (!promotionProduct.hasQuantity(purchase.getQuantity())) {
+            //일반재고로 구매할건지 묻기
+        }
+    }
+
+    private static void checkRegularQuantity(Purchase purchase, Products products) {
+        Product regularProduct = products.getRegularProducts().get(purchase.getName());
+        if (!regularProduct.hasQuantity(purchase.getQuantity())) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ENOUGH_QUANTITY_ERROR.getMessage());
+        }
+    }
+}
