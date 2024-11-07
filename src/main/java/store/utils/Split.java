f29ec761eb16006c3005272273df069a86b1f8ae
bmg1203 <bmg1203@naver.com>
2024-11-07 20:16:04 +0900

    [feat]: Split 클래스 대괄호 및 하이픈 split 기능 추가
    - 문자열 리스트의 대괄호를 제거합니다.
    - 문자열의 하이픈을 제거하여 리스트로 반환합니다.

--- src/main/java/store/utils/Split.java
+++ src/main/java/store/utils/Split.java
@@ -1,12 +1,27 @@
 package store.utils;
 
+import java.util.ArrayList;
 import java.util.List;
+import store.domain.Purchase;
 
 public class Split {
 
     private static final String COMMA = ",";
+    private static final String HYPHEN = "-";
 
     public static List<String> commaSpliter(String str) {
         return List.of(str.split(COMMA));
+    }
+
+    public static List<String> squareBracketsSpliter(List<String> purchase) {
+        for (String product : purchase) {
+            product.replaceAll("\\[", "");
+            product.replaceAll("\\]", "");
+        }
+        return purchase;
+    }
+
+    public static List<String> hyphenSpliter(String str) {
+        return List.of(str.split(HYPHEN));
     }
 }
