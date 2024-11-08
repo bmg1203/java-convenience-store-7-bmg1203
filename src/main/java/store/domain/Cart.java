51a38126387d211eb4498320a06c4fbec51fecdb
bmg1203 <bmg1203@naver.com>
2024-11-09 02:01:04 +0900

    [fix]: Cart addItems 메소드 수정 및 일부 수정
    - items의 final 수정
    - addPurchase에 매개변수 수정
    - addItems 존재 구분 확인 방식 수정

--- src/main/java/store/domain/Cart.java
+++ src/main/java/store/domain/Cart.java
@@ -8,48 +8,49 @@ import store.constants.ErrorMessage;
 
 public class Cart {
 
-    private final List<Purchase> items;
+    private List<Purchase> items;
 
     public Cart(List<Purchase> items) {
         this.items = new ArrayList<>();
-        addPurchase(items);
+        addPurchase(new ArrayList<>(items));
     }
 
     public List<Purchase> getItems() {
         return items;
     }
 
-    private void addPurchase(List<Purchase> items) {
-        for (Purchase purchase : items) {
+    private void addPurchase(List<Purchase> purchases) {
+        for (Purchase purchase : purchases) {
             if(validateItem(purchase)) {
-                items.add(purchase);
+                this.items.add(purchase);
             }
         }
     }
 
     private boolean validateItem(Purchase purchase) {
-        if (items.contains(purchase)) {
+        if (items.stream().anyMatch(item -> item.getName().equals(purchase.getName()))) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_PRODUCT_INPUT_ERROR.getMessage());
         }
         return true;
     }
 
     public Map<String, Purchase> mergeItems(List<Purchase> purchases) {
-        Map<String, Purchase> items = new HashMap<>();
+        Map<String, Purchase> itemsMap = new HashMap<>();
         for (Purchase purchase : purchases) {
-            addItems(purchase, items);
+            addItems(purchase, itemsMap);
         }
-        return items;
+        return itemsMap;
     }
 
-    private static void addItems(Purchase purchase, Map<String, Purchase> items) {
-        if (items.containsKey(purchase.getName())) {
-            Purchase item = items.get(purchase.getName());
-            item.setQuantity(item.getQuantity() + purchase.getQuantity());
+    private static void addItems(Purchase purchase, Map<String, Purchase> itemsMap) {
+        Purchase existingItem = itemsMap.get(purchase.getName());
+
+        if (existingItem != null) {
+            existingItem.setQuantity(existingItem.getQuantity() + purchase.getQuantity());
         }
-        if (!items.containsKey(purchase.getName())) {
+        if (existingItem == null) {
             Purchase item = new Purchase(purchase.getName(), purchase.getQuantity(), purchase.getPromotion());
-            items.put(item.getName(), item);
+            itemsMap.put(item.getName(), item);
         }
     }
 }
