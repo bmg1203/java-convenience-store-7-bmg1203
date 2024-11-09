f2299d4c53ee0278f3830962a9deceea719a3077
bmg1203 <bmg1203@naver.com>
2024-11-10 01:40:53 +0900

    [fix]: PromotionService 프로모션 처리 오류 수정
    - 프로모션 개수에 맞지 않을 때 처리
    - 프로모션 재고 부족시 처리
    - 카트 변경 기능

--- src/main/java/store/service/PromotionService.java
+++ src/main/java/store/service/PromotionService.java
@@ -1,5 +1,7 @@
 package store.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import store.domain.Cart;
 import store.domain.Product;
 import store.domain.Products;
@@ -10,58 +12,81 @@ import store.view.InputView;
 
 public class PromotionService {
 
+    private static final String NO_PROMOTION = "null";
+    private static final String YES = "Y";
+    private static final String NO = "N";
     private final InputView inputView;
+    private static final List<Purchase> addPurchase = new ArrayList<>();
 
     public PromotionService(InputView inputView) {
         this.inputView = inputView;
     }
 
-    public boolean isPromotionWithinPeriod(String promotionName, Promotions promotions) {
-        Promotion promotion = promotions.getPromotions().get(promotionName);
-        if (promotion == null) {
-            return false;
+    //프로모션 있으면 set
+    public void setPromotion(Purchase purchase, Products products) {
+        Product product = products.getPromotionProducts().get(purchase.getName());
+        if (product != null) {
+            purchase.setPromotion(product.getPromotion());
         }
-        return promotion.isActive();
     }
 
     //프로모션 종류별 혜택 개수 맞는지 확인
     //프로모션 재고 부족시 일반 재고(정상가)로 구매건지 확인
     //나머진 그냥 구매로 굳히기(카트에 들어가는 것인 Purchase에도 프로모션 넣자)
     public void promotionsAll(Purchase purchase, Promotions promotions, Products products, Cart cart) {
-        //종류별 혜택 개수 확인
-        Promotion promotion = promotions.getPromotions().get(purchase.getName());
-        Product product = products.getPromotionProducts().get(purchase.getName());
-        purchase.setPromotion(promotion.getName());
+        Promotion promotion = promotions.getPromotions().get(purchase.getPromotion());
+        if (promotion == null) {
+            return;
+        }
 
-        //프로모션 개수만큼 가져왔는가?
+        adjustPromotionCount(purchase, promotion);
+        handleInsufficientStock(purchase, cart, products, promotion);
+    }
+
+    private void adjustPromotionCount(Purchase purchase, Promotion promotion) {
         if (!promotion.correctCount(purchase.getQuantity())) {
-            String answer = inputView.checkMorePurchase();
-            if (answer.equals("Y")) {
+            String answer = inputView.checkPromotionCountAdd(purchase);
+            if (answer.equals(YES)) {
                 int more = promotion.addCount(purchase.getQuantity());
                 purchase.setQuantity(more);
             }
-            if (answer.equals("N")) {
-                purchase.setQuantity(purchase.getQuantity() - promotion.extraCount(purchase.getQuantity()));
-            }
-        }
-
-        //프로모션 재고가 부족한가?
-        if (product.getQuantity() < purchase.getQuantity()) {
-            String answer = inputView.checkPromotionSaleNotAccept(purchase, promotion.extraCount(purchase.getQuantity()));
-            if (answer.equals("Y")) {
-                int extraCount = promotion.extraCount(purchase.getQuantity());
-                purchase.setQuantity(purchase.getQuantity() - extraCount);
-                //일반 상품으로 추가
-                cart.getItems().add(new Purchase(purchase.getName(), purchase.getQuantity(), promotion.getName()));
-            }
-            if (answer.equals("N")) {
+            if (answer.equals(NO)) {
                 purchase.setQuantity(purchase.getQuantity() - promotion.extraCount(purchase.getQuantity()));
             }
         }
     }
 
-    public int calculatePromotionQuantity(int quantity, Promotion promotion) {
-        int applicableQuantity = (quantity / promotion.getBuy()) * promotion.getGet();
-        return applicableQuantity;
+    private void handleInsufficientStock(Purchase purchase, Cart cart, Products products, Promotion promotion) {
+        Product product = products.getPromotionProducts().get(purchase.getName());
+        if (product.getQuantity() < purchase.getQuantity()) {
+            int extraCount = purchase.getQuantity() - (product.getQuantity() - promotion.extraCount(product.getQuantity()));
+            String answer = inputView.checkPromotionSaleNotAccept(purchase, extraCount);
+            handleByAnswerInsufficientStock(purchase, cart, promotion, product, answer, extraCount);
+        }
+    }
+
+    private static void handleByAnswerInsufficientStock(Purchase purchase, Cart cart, Promotion promotion, Product product, String answer,
+                                                        int extraCount) {
+        int promotionQuantity = purchase.getQuantity() - extraCount;
+
+        if (answer.equals(YES)) {
+            purchase.setQuantity(promotionQuantity);
+            // 일반 상품으로 추가
+            addPurchase.add(new Purchase(purchase.getName(), extraCount, "null"));
+        }
+        if (answer.equals(NO)) {
+            purchase.setQuantity(promotionQuantity);
+        }
+    }
+
+    public void applyAddPurchaseToCart(Cart cart) {
+        if (addPurchase != null && !addPurchase.isEmpty()) {
+            cart.getItems().addAll(addPurchase);
+            addPurchase.clear();
+        }
+    }
+
+    public static void clearAddCart() {
+        addPurchase.clear();
     }
 }
