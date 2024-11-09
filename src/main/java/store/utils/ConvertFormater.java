4e5eff6a52037a3c899d27b39e81654e5268d643
bmg1203 <bmg1203@naver.com>
2024-11-10 01:37:26 +0900

    [style]: ConvertFormater 상수를 변수로 교체
    - 하드코딩 되어있는 상수를 변수로 교체합니다.

--- src/main/java/store/utils/ConvertFormater.java
+++ src/main/java/store/utils/ConvertFormater.java
@@ -4,6 +4,9 @@ import java.text.DecimalFormat;
 
 public class ConvertFormater {
 
+    private static final String COUNT_UNIT = "개";
+    private static final String ZERO_QUANTITY = "재고 없음";
+
     public static String moneyFormat(int money) {
         DecimalFormat df = new DecimalFormat("###,###");
         String formatMoney = df.format(money);
@@ -12,8 +15,8 @@ public class ConvertFormater {
 
     public static String quantityFormat(int quantity) {
         if (quantity == 0) {
-            return "재고 없음";
+            return ZERO_QUANTITY;
         }
-        return String.valueOf(quantity);
+        return String.valueOf(quantity) + COUNT_UNIT;
     }
 }
