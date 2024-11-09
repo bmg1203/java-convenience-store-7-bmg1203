97aa68cbbea8ff0d17b044a968dba9cef30fb219
bmg1203 <bmg1203@naver.com>
2024-11-10 01:59:51 +0900

    [style]: InputPrompts 줄바꿈 수정
    - 줄바꿈이 출력형태와 맞도록 수정합니다.

--- src/main/java/store/constants/InputPrompts.java
+++ src/main/java/store/constants/InputPrompts.java
@@ -6,7 +6,7 @@ public enum InputPrompts {
     ENTER_PURCHASE_PRODUCTS("구매하실 상품명과 수량을 입력해주세요. (예: [사이다-2],[감자칩-1])"),
     PROMOTION_QUANTITY_ADD("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n"),
     NOT_ENOUGH_PROMOTION_QUANTITY("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n"),
-    MEMBERSHIP_DISCOUNT("멤버십 할인을 받으시겠습니까? (Y/N)"),
+    MEMBERSHIP_DISCOUNT("\n멤버십 할인을 받으시겠습니까? (Y/N)"),
     MORE_PURCHASE("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
 
     private final String prompt;
