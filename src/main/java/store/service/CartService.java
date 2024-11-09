6c0ea8e76ced6f989afc91283175ba9b332d32d6
bmg1203 <bmg1203@naver.com>
2024-11-10 01:49:03 +0900

    [feat]: CartService 재고 감소 기능 추가 및 필요없는 기능 제거
    - 장바구니의 재고를 이용해 재고를 감소합니다.
    - 필요없는 기능들을 제거합니다.

--- src/main/java/store/service/CartService.java
+++ src/main/java/store/service/CartService.java
@@ -1,48 +1,20 @@
 package store.service;
 
-import store.constants.ErrorMessage;
 import store.domain.Cart;
-import store.domain.Product;
 import store.domain.Products;
 import store.domain.Purchase;
-import store.view.InputView;
 
 public class CartService {
 
-    private final InputView inputView;
-
-    public CartService(InputView inputView) {
-        this.inputView = inputView;
-    }
-
-    public void hasQuantity(Cart cart, Products products, PromotionService promotionService) {
+    public Products productsReduce(Products products, Cart cart) {
         for (Purchase purchase : cart.getItems()) {
-            if (isPromotionProduct(purchase, products, promotionService)) {
-                checkPromotionQuantity(purchase, products);
-                continue;
+            if (purchase.getPromotion().equals("null")) {
+                products.getRegularProducts().get(purchase.getName()).updateQuantity(purchase.getQuantity());
             }
-            if (products.getRegularProducts().containsKey(purchase.getName())) {
-                checkRegularQuantity(purchase, products);
+            if (!purchase.getPromotion().equals("null")) {
+                products.getPromotionProducts().get(purchase.getName()).updateQuantity(purchase.getQuantity());
             }
         }
-    }
-
-    private static boolean isPromotionProduct(Purchase purchase, Products products, PromotionService promotionService) {
-        return products.getPromotionProducts().containsKey(purchase.getName())
-                && promotionService.isPromotionWithinPeriod(products.getPromotionProducts().get(purchase.getName()).getPromotion());
-    }
-
-    private void checkPromotionQuantity(Purchase purchase, Products products) {
-        Product promotionProduct = products.getPromotionProducts().get(purchase.getName());
-        if (!promotionProduct.hasQuantity(purchase.getQuantity())) {
-            inputView.checkPromotionSaleNotAccept(purchase);
-        }
-    }
-
-    private void checkRegularQuantity(Purchase purchase, Products products) {
-        Product regularProduct = products.getRegularProducts().get(purchase.getName());
-        if (!regularProduct.hasQuantity(purchase.getQuantity())) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_ENOUGH_QUANTITY_ERROR.getMessage());
-        }
+        return products;
     }
 }
