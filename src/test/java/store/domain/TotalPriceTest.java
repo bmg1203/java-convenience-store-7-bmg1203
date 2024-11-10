ea6020c47a1c4dae4d1ee19ab66b8ac1ff9d9816
bmg1203 <bmg1203@naver.com>
2024-11-10 21:20:10 +0900

    [test]: TotalPrice 멤버십 할인 메소드 변경에 따른 수정

--- src/test/java/store/domain/TotalPriceTest.java
+++ src/test/java/store/domain/TotalPriceTest.java
@@ -20,10 +20,13 @@ class TotalPriceTest {
         allProducts.add(new Product("콜라", "1500", "10", "탄산2+1"));
         allProducts.add(new Product("콜라", "1500", "10", "null"));
         allProducts.add(new Product("사이다", "1300", "15", "null"));
+        allProducts.add(new Product("감자칩", "1500", "15", "반짝할인"));
+        allProducts.add(new Product("감자칩", "1500", "15", "null"));
         products = new Products(allProducts);
 
         List<Promotion> promotionList = new ArrayList<>();
         promotionList.add(new Promotion("탄산2+1", "2", "1", "2024-01-01", "2024-12-31"));
+        promotionList.add(new Promotion("반짝할인", "1", "1", "2024-01-01", "2024-12-31"));
         promotions = new Promotions(promotionList);
 
         purchases = new ArrayList<>();
@@ -66,7 +69,7 @@ class TotalPriceTest {
     @Test
     void 멤버십_할인_금액_테스트() {
         // when
-        int calculatedMembershipPrice = (int) totalPrice.getMembershipSalePrice(purchases, promotions, products);
+        int calculatedMembershipPrice = (int) totalPrice.getMembershipSalePrice(purchases, products);
 
         // then
         // 총 가격 14200 - 프로모션 할인 3000 = 11200 -> 멤버십 할인 적용 시 최대 8000원
@@ -86,5 +89,22 @@ class TotalPriceTest {
 
         // then
         assertThat(calculatedPromotionPrice).isEqualTo(0);
+    }
+
+    @Test
+    void 프로모션_적용_있는_상품_멤버십_할인_테스트() {
+        // given
+        List<Purchase> purchases = new ArrayList<>();
+        purchases.add(new Purchase("감자칩", 2, "반짝할인"));
+
+        TotalPrice totalPrice = new TotalPrice(purchases, products);
+
+        // when
+        int calculatedPromotionPrice = totalPrice.getPromotionSalePrice(purchases, promotions, products);
+        double calculatedMembershipPrice = totalPrice.getMembershipSalePrice(purchases, products);
+
+        // then
+        assertThat(calculatedPromotionPrice).isEqualTo(1500);
+        assertThat(calculatedMembershipPrice).isEqualTo(0);
     }
 }
